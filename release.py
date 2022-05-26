c211d70afa938744f652ef69f9b8dcee310352a5
Shujia Huang <huangshujia9@gmail.com>
2022-05-26 19:24:51 +0800

    commit a release.py

---
+++ release.py
@@ -0,0 +1,19 @@
+"""Release and publish genotools to PyPI.
+
+Author: Shujia Huang
+Date: 2022-05-26
+"""
+import importlib
+from subprocess import call
+
+spec = importlib.util.spec_from_file_location("_", "./setup.py")
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
+# pip install twine
+# python setup.py register
+
+call(["python", "setup.py", "sdist"])
+tarball = "dist/{}-{}.tar.gz".format(module.meta.__DISTNAME__, module.meta.__VERSION__)
+call(["twine", "upload", "-r", "pypi", tarball])
+
